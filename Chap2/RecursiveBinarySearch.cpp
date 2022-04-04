22d3fbabbe65b05fdd0dd22fa400663664def1b4
LSH3333 <dltpgustpgus@gmail.com>
2022-04-04 15:10:54 +0900

    2022.04.04

---
+++ Chap2/RecursiveBinarySearch.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+int BinarySearchRecur(int ar[], int first, int last, int target)
+{
+    if(first > last) return -1; // 실패
+
+    int mid = (first + last) / 2;
+    if(ar[mid == target]) return mid;
+    else if(target < ar[mid])
+        return BinarySearchRecur(ar, first, mid-1, target);
+    else
+        return BinarySearchRecur(ar, mid+1, last, target);
+}
+
+int main()
+{
+    int arr[] = {1, 3, 5, 7, 9};
+    int idx;
+    idx = BinarySearchRecur(arr, 0, sizeof(arr)/sizeof(int)-1, 7);
+
+}
